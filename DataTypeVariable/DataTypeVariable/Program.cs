cf0f0b1b5d580c04527b803ad17b041c0aa9387a
bahruzz <bahruzea@code.edu.az>
2026-03-03 10:58:05 +0400

    Added

---
+++ DataTypeVariable/DataTypeVariable/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//string name ="Ravena";
+//string surname = "Balagozova";
+//int age = 21;
+//bool isStudent = true;
+
+//Console.WriteLine(name);
+//Console.WriteLine(surname);	
+//Console.WriteLine(age);
+//Console.WriteLine(isStudent);
+
+int x = 5;
+int y = 3;
+float z = (float)x / y;
+
+      Console.WriteLine(z);
